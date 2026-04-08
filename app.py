385965ecc3a52d2a979276e5cc1882b308856458
Berlin <berlinyesubatham@gmail.com>
2026-04-08 17:06:57 +0530

    Render ready version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, session
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "secret123"
@@ -80,8 +81,7 @@ def logout():
     session.clear()
     return redirect("/login")
 
-app.run(debug=True)
-
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
